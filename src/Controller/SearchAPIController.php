178aeeea5d27847c59e3eca0a88dd41c1f2034b1
Nguyen Maurice <mnguyen@adimeo.com>
2023-09-15 10:53:51 +0200

    Fix pinned documents

--- src/Controller/SearchAPIController.php
+++ src/Controller/SearchAPIController.php
@@ -933,7 +933,7 @@ class SearchAPIController extends AdimeoDataSuiteController
         // Get pinned documents
         $query = $this->queryManager->setPinnedDocuments($query, $store_uid);
 
-        //return $query;
+        return $query;
 
         //echo '<pre>';
         //echo json_encode($query); die;
