3a6c8abcd9686eae646c522db3fbf387dbd536ba
Nguyen Maurice <mnguyen@adimeo.com>
2023-12-15 17:31:28 +0100

    Fix bug Hermes

--- src/Controller/SearchAPIController.php
+++ src/Controller/SearchAPIController.php
@@ -540,7 +540,7 @@ class SearchAPIController extends AdimeoDataSuiteController
                     $query['_source']['includes'] = array_map('trim', explode(',', $request->get('include_fields')));
                 }
 
-                if (in_array($indexName, ['pdb_product', 'product', 'products']) && intval($query_string) === 0) {
+                if (in_array($indexName, ['pdb_product', 'product', 'products']) && intval($query_string) === 0 && !str_contains($query_string, 'category_id')) {
                     $query = $this->finalizeQuery($query, $store_uid);
                 }
 
